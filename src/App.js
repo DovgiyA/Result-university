1366e32f9c8febf6f86aa11c210bf96886e1ab5c
Alex <black.s.w.a.n@yandex.ru>
2024-03-12 15:35:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useFetch } from './useFetch';
+
+
 
 function App() {
+  const {
+    data,
+    isLoading,
+    error,
+    refetch
+  } = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+  console.log({
+    data,
+    isLoading,
+    error,
+    refetch
+  })
+	
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => refetch({
+          params: {
+            _limit: 3
+          }
+        })}>
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && 'Загрузка...'}
+      {error && 'Произошла ошибка'}
+      {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>) }
     </div>
   );
 }
